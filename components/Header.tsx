19c6a1ba2f74d42ecea165d8c63c597897d221c6
lucasoneves <casluhc@gmail.com>
2025-04-29 18:59:39 -0300

    Novo logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,19 @@
 import Link from "next/link";
 import MenuList from "./MenuList";
+import Image from "next/image";
 
 export default function Header() {
   return (
     <header className="w-full py-4 bg-neutral-900 px-4 shadow-md sticky top-0 z-50">
       <nav className="flex justify-between items-center max-w-5xl mx-auto">
         <Link href="/" className="text-lg sm:text-xl font-bold">
-          Lucas Neves
+          <Image
+            src="/logotipo-lucasneves.png"
+            alt="lucas neves"
+            width={40}
+            height={40}
+            className="rounded-full"
+          />
         </Link>
         <MenuList />
       </nav>
